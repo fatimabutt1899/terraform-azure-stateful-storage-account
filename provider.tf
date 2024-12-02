1589a6262db8e1e824b25e88b5f2bf5eb2e1878a
Fatima <fatimabutt18899@gmail.com>
2024-12-02 18:11:53 +0500

    first commit

---
+++ provider.tf
@@ -0,0 +1,10 @@
+# provider "azurerm" {
+#   features {}
+
+#   client_id       = "YOUR_APP_ID"
+#   client_secret   = "YOUR_CLIENT_SECRET"
+#   tenant_id       = "YOUR_TENANT_ID"
+#   subscription_id = "YOUR_SUBSCRIPTION_ID"
+# }
+
+
